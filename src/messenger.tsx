f8a77490756ac527891180db96c54eba365437d0
John Schmiel <johnschml@verizon.net>
2021-08-10 00:19:25 -0400

    Added logged in user and their last selected symbols and shares to bearer token response

--- src/messenger.tsx
+++ src/messenger.tsx
@@ -1,34 +1,56 @@
 import { StockPosition, DividendRequestData } from './interfaces';
+var CryptoJS = require("crypto-js");
 
 export function getBearerToken(): Promise<string> {
 	return new Promise((resolve, reject) => {
-		// let requestUrl = 'http://localhost:8080/v1/user?user=' + process.env.REACT_APP_USERNAME +
-		let requestUrl = 'https://ibo-financials.com/v1/user?user=' + process.env.REACT_APP_USERNAME +
-		//let requestUrl = 'http://192.168.1.7:8080/v1/user?user=' + process.env.REACT_APP_USERNAME +
-			'&password=' + process.env.REACT_APP_PASSWORD;
+		let requestUrl = 'http://localhost:8080/v1/token';
+		// let requestUrl = 'https://ibo-financials.com/v1/token';
+		// let requestUrl = 'http://localhost:8080/v1/user?user=' + process.env.REACT_APP_USERNAME + '&password=' + process.env.REACT_APP_PASSWORD;
+		// let requestUrl = 'https://ibo-financials.com/v1/user?user=' + process.env.REACT_APP_USERNAME + '&password=' + process.env.REACT_APP_PASSWORD;
+		// let requestUrl = 'http://192.168.1.7:8080/v1/user?user=' + process.env.REACT_APP_USERNAME + '&password=' + process.env.REACT_APP_PASSWORD;
 
-		fetch(requestUrl, { method: 'POST' })
+		let tokenJson = JSON.stringify({userName : process.env.REACT_APP_USERNAME, password : process.env.REACT_APP_PASSWORD, issueDate : Date.now()});
+		let token = encrypt(tokenJson);
+
+		fetch(requestUrl, { 
+			method: 'POST', 
+			headers: { 
+				"Content-Type" : "text/plain"
+			  , "X-Forwarded-Remote-User" : "jgslfs" 
+		    },
+			body: token
+		})
 			.then(response => response.json())
-			.then(data => { resolve( data.token) });
+			.then(data => { resolve( data ) });
 	});
 }
 
-export async function getDividendPayments(stockPositions: StockPosition[], bearerToken: string): Promise<DividendRequestData> {
+export async function getDividendPayments(data: any): Promise<DividendRequestData> {
 	return new Promise((resolve, reject) => {
-		// let requestUrl = 'http://localhost:8080/v1/dividends/calendar/';
-		let requestUrl = 'https://ibo-financials.com/v1/dividends/calendar/';
+		let requestUrl = 'http://localhost:8080/v1/dividends/calendar/';
+		//let requestUrl = 'https://ibo-financials.com/v1/dividends/calendar/';
 		//let requestUrl = 'http://192.168.1.7:8080/v1/dividends/calendar/';
-		let symbolQuery = '';
-		let sharesQuery = '';
+		let symbolQuery = data.symbols;
+		let sharesQuery = data.shares;
 
-		stockPositions.forEach((position) => {
-			symbolQuery += position.symbol + ',';
-			sharesQuery += position.shares + ',';
+		// stockPositions.forEach((position) => {
+		// 	symbolQuery += position.symbol + ',';
+		// 	sharesQuery += position.shares + ',';
+		// })
+
+		let bearerToken = data.token;
+
+		requestUrl += symbolQuery + '/' + sharesQuery + '/date?user=' + data.user;
+
+		fetch(requestUrl, { 
+			headers: { Authorization: bearerToken } 
 		})
-
-		requestUrl += symbolQuery + '/' + sharesQuery + '/date';
-
-		fetch(requestUrl, { headers: { Authorization: bearerToken } }).then(request => request.json()).then(data => { resolve( data) });
+			.then(request => request.json())
+			.then(data => { resolve( data) });
 
 	});
+}
+
+export function encrypt(value: String): any {
+	return CryptoJS.AES.encrypt(value, process.env.REACT_APP_SECRET).toString();
 }
