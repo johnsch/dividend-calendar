82c6bac6df7ff5a95599195eee4e88581767197d
Eric Schmiel <eschmiel@gmail.com>
2021-08-30 16:03:24 -0400

    rigged up the DividendSearch form as a controlled component

--- src/dividendSearch.tsx
+++ src/dividendSearch.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {DividendPayment, StockPosition} from './interfaces';
 import TrackedStockPosition from './trackedStockPosition';
 
@@ -8,7 +8,27 @@ type dividendSearchProps = {
 }
 
 export default function DividendSearch({dividendPayments, /*changeDividendData*/}: dividendSearchProps){
-	
+
+	const [formSymbol, changeFormSymbol] = useState('');
+	const [formShares, changeFormShares] = useState<number>();
+
+	function handleFormChange(event: React.FormEvent<HTMLInputElement> & { target: HTMLInputElement }) {
+		switch (event.target.name) { 
+			case 'symbol': changeFormSymbol(event.target.value);
+				break;
+
+			case 'shares':
+				let newFormShares = Number(event.target.value);
+				if (isNaN(newFormShares))
+					newFormShares = 0;
+					changeFormShares(newFormShares);
+				break;
+
+			default:
+				throw new Error();
+		}
+    }
+
 	let stockPositions: JSX.Element[] = [];
 	let loadedStocks: string[] = [];
 
@@ -28,8 +48,9 @@ export default function DividendSearch({dividendPayments, /*changeDividendData*/
 	return (
 		<div id='dividendSearch'>
 			<form>
-				<label htmlFor='symbol'>Stock Symbol: </label> <input type='text' id='symbol' name='symbol' />
-				<label htmlFor='shares'>Shares: </label> <input type='text' id='shares' name='shares' />
+				<label htmlFor='symbol'>Stock Symbol: </label> <input type='text' id='symbol' name='symbol' value={formSymbol} onChange={handleFormChange} />
+				<label htmlFor='shares'>Shares: </label> <input type='text' id='shares' name='shares' value={formShares} onChange={handleFormChange} />
+				<input type='submit' value='Track ' />
 			</form>
 
 			<div className='trackedStockPosition-container'>
