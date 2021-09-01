779eab59305beecde5767eb0568fd380d54470c8
eschmiel <eschmiel@gmail.com>
2021-09-01 15:21:34 -0400

    added symbol and share labels to trackedStockPosition components and JSX for a remove position button

--- src/main/dividend search/trackedStockPosition.tsx
+++ src/main/dividend search/trackedStockPosition.tsx
@@ -3,10 +3,15 @@ import {StockPosition} from '../mainTypes';
 import './trackedStockPosition.css';
 
 export default function TrackedStockPosition({symbol, shares}: StockPosition){
-	return(
+	return (
 		<div className='trackedStockPosition'>
-			<div>{symbol}</div>
-			<div>{shares}</div>
+			<div className='trackedStockPositionData'>
+				<div className='trackedStockPositionValue'>{symbol}</div>
+				<div className='trackedStockPositionValue'>{shares}</div>
+				<div className='trackedStockPositionLabel'>symbol</div>
+				<div className='trackedStockPositionLabel'>shares</div>
+			</div>
+			<div className='trackedStockPositionDeleteButton'>X</div>
 		</div>
 	);
 }
