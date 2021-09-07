61294eb378db0098df377202d33e8502dd85369b
John Schmiel <johnschml@verizon.net>
2021-09-07 18:18:18 -0400

    Updated urls to localhost

--- src/messenger/messenger.tsx
+++ src/messenger/messenger.tsx
@@ -11,9 +11,9 @@ var CryptoJS = require("crypto-js");
 
 export function getBearerToken(): Promise<TokenRequestData> {
 	return new Promise((resolve, reject) => {
-		let requestUrl = 'https://ibo-financials.com/v1/token';
+		// let requestUrl = 'https://ibo-financials.com/v1/token';
 		// let requestUrl = 'http://192.168.1.7:7070/v1/token';
-		// let requestUrl = 'http://localhost:7070/v1/token';
+		let requestUrl = 'http://localhost:7070/v1/token';
 
 		let tokenJson = JSON.stringify({userName : process.env.REACT_APP_USERNAME, password : process.env.REACT_APP_PASSWORD, issueDate : Date.now()});
 		let token = encrypt(tokenJson);
@@ -34,31 +34,12 @@ export function getBearerToken(): Promise<TokenRequestData> {
 	});
 }
 
-export async function getUserSymbolsShares(data: TokenRequestData): Promise<UserSymbolsShares> {
-	return new Promise((resolve, reject) => {
-		let requestUrl = 'https://ibo-financials.com/v1/userSymbolsShares';
-		// let requestUrl = 'http://192.168.1.7:7070/v1/userSymbolsShares';
-
-		let headers = new Headers();
-		headers.append('Content-Type', 'application/json');
-
-		fetch(requestUrl, { 
-			method: 'POST', 
-			headers: headers,
-			redirect: 'follow'
-		})
-		.then(request => request.json())
-		.then(data => { resolve( data) })
-		.catch(error => console.log('error', error));
-	});
-}
-
 
 export async function getDividendPayments(stockPositions: StockPosition[], bearerTokenData: BearerTokenData, user: string): Promise<DividendPaymentResponseData> {
 	return new Promise((resolve, reject) => {
-		let requestUrl = 'https://ibo-financials.com/v1/dividends/calendar/';
+		// let requestUrl = 'https://ibo-financials.com/v1/dividends/calendar/';
 		// let requestUrl = 'http://192.168.1.7:7070/v1/dividends/calendar/';
-		// let requestUrl = 'http://localhost:7070/v1/dividends/calendar/';
+		let requestUrl = 'http://localhost:7070/v1/dividends/calendar/';
 
 		let symbolQuery = '';
 		let sharesQuery = '';
@@ -86,6 +67,25 @@ export async function getDividendPayments(stockPositions: StockPosition[], beare
 		.then(data => { resolve( data) })
 		.catch(error => console.log('error', error));
 
+	});
+}
+
+export async function getUserSymbolsShares(data: TokenRequestData): Promise<UserSymbolsShares> {
+	return new Promise((resolve, reject) => {
+		let requestUrl = 'https://ibo-financials.com/v1/userSymbolsShares';
+		// let requestUrl = 'http://192.168.1.7:7070/v1/userSymbolsShares';
+
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+
+		fetch(requestUrl, { 
+			method: 'POST', 
+			headers: headers,
+			redirect: 'follow'
+		})
+		.then(request => request.json())
+		.then(data => { resolve( data) })
+		.catch(error => console.log('error', error));
 	});
 }
 
